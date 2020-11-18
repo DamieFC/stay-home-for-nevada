f392f9f53e6fecc29a5144d813125dd15bc9565f
DFC <69485115+DamieFC@users.noreply.github.com>
2020-11-18 13:24:31 -0800

    Update shfn.java

--- shfn.java
+++ shfn.java
@@ -1,6 +1,6 @@
 // I got the code off of w3schools.com
 public class MyClass {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Stay home for Nevada");
   }
 }
