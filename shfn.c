6b50d0c1e59d46a68e27469c2923f5167fd14782
DFC <69485115+DamieFC@users.noreply.github.com>
2021-02-08 09:33:39 -0800

    Update shfn.c

--- shfn.c
+++ shfn.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 int main(){
-  printf("Stay home for Nevada--AND I MEAN IT");
+  printf("Stay home for Nevada--AND I MEAN IT! Unless you want to die a long and painful death with a tube up your nose.");
 }
